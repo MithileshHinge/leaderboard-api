bd7c2df2794097f94608a6fa578f1d232d0684ba
Mithilesh Hinge <mithhinge@gmail.com>
2023-01-03 04:28:48 +0530

    POST /pnl e2e test todos added;

---
+++ src/__test__/e2e/postPnL.e2e.test.ts
@@ -0,0 +1,6 @@
+describe('POST /pnl e2e tests', () => {
+	it.todo('User can successfully submit PnL');
+	it.todo('Respond with 401 (Unauthorized) if client is not authenticated');
+	it.todo('Respond with 400 (Bad Request) if PnL value is < -100');
+	it.todo('Respond with 400 (Bad Request) if PnL already submitted');
+});
